39616985ee207e0af9fe28b9a9a97b5e65cb4542
lively-teacher-9822 <ccrive10@gmail.com>
2018-12-05 16:25:21 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return ['foo', ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift('foo');
+}
+
+function addElementToEndOfArray
